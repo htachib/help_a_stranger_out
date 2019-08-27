b5e725cea038036ccbf42792c27daad87bd49b4a
Hideko <hideko.tachibana@gmail.com>
2019-08-27 14:31:15 +0200

    object oriented clean up

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,21 @@
+function findParentBySelector(elm, selector) {
+  var all = document.querySelectorAll(selector);
+  var cur = elm.parentNode;
+  while(cur && !collectionHas(all, cur)) { //keep going up until you find a match
+      cur = cur.parentNode; //go up
+  }
+  return cur; //will return null if not found
+}
+
+function collectionHas(a, b) { //helper function (see below)
+  for(var i = 0, len = a.length; i < len; i ++) {
+      if(a[i] == b) return true;
+  }
+  return false;
+}
+
+function hideByClass(elements) {
+  for (let el of elements) {
+    el.classList.add('hide');
+  }
+}

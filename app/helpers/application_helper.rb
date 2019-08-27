0781001479868d1f29d66c73a3725c52538c4c5d
Hideko <hideko.tachibana@gmail.com>
2019-08-27 15:12:59 +0200

    nav bar active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def active_tab(matcher)
-    request.env['REQUEST_PATH'] == matcher ? 'is-active' : ''
-  end
-
   def user_signed_in?
     # binding.pry
     # ExecJS.eval "blockstackSession.isUserSignedIn()"

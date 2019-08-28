f20fabff40fec7be24db2b2b206a2ac9f049ce9d
Hideko <hideko.tachibana@gmail.com>
2019-08-28 23:14:06 +0200

    quotes and inspire and why pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,11 @@ class PagesController < ApplicationController
   end
 
   def inspire_me
+    quote = Quotes::Base::QUOTES.sample
+    @text = quote[:body]
+    @author = quote[:author]
+    image_id = (1..20).to_a.sample
+    @image_path = "/quotes/#{image_id}.png"
   end
 
   def why
